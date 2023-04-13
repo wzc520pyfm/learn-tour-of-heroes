c7d72cc36c5229e425e319a927b34125d91a8f2b
wzc520pyfm <1528857653@qq.com>
2023-04-13 13:48:45 +0800

    feat: add update hero feature

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   // 服务中的服务(在heroService中注入messageService)
   constructor(
     private http: HttpClient,
@@ -34,6 +38,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     )
   }
 
